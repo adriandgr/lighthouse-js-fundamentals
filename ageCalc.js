f09f1e75b4b425fbcf80aa0bea48179e082e0b0f
Adrian Diaz <web.art@adriandiaz.ca>
2017-01-19 04:35:12 +0000

    adds conditional ex.

---
+++ ageCalc.js
@@ -0,0 +1,8 @@
+function ageCalculator(name, yearOfBirth, currentYear) {
+  var age = currentYear - yearOfBirth;
+  return name + " is " + age + " years old.";
+}
+
+console.log(ageCalculator("Julian", 1964, 2017));
+console.log(ageCalculator("Miranda", 1983, 2017));
+console.log(ageCalculator("Ferdinand", 1988, 2017));
